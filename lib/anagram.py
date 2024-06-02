988c4c96f68d78ef98c527c29fd84efbf7d3f5a7
Jazper <jazpersaldana@yahoo.com>
2024-06-02 11:46:46 -0600

    anagram detector lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        return_list = []
+        for list_word in list:
+            list_letters = [letter for letter in list_word]
+            word_letters = [letter for letter in self.word]
+            if sorted(list_letters) == sorted(word_letters):
+                return_list.append(list_word)
+        return return_list
+
+# breakpoint()
